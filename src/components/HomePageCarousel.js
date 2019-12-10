f576d9fb1e05ab1f0d4fa369c7c432cad81d40a5
atapp <sihogg@bu.edu>
2019-12-10 12:00:51 -0500

    hgf

--- src/components/HomePageCarousel.js
+++ src/components/HomePageCarousel.js
@@ -18,29 +18,11 @@ const HomePageCarousel = () => {
           <div className="mask rgba-black-light d-flex justify-content-center align-items-center">
             <div className="text-center white-text mx-5 wow fadeIn">
               <h1 className="mb-4">
-                <strong>Learn Bootstrap 4 with MDB</strong>
-              </h1>
-
-              <p>
-                <strong>Best free guide of responsive web design</strong>
-              </p>
-
-              <p className="mb-4 d-none d-md-block">
                 <strong>
-                  The most comprehensive tutorial for the Bootstrap 4. Loved by
-                  over 500 000 users. Video and written versions available.
-                  Create your own, stunning website.
+                  Join our community, where music brings us closer and talent
+                  makes us bigger
                 </strong>
-              </p>
-
-              <a
-                target="_blank"
-                href="https://mdbootstrap.com/education/bootstrap/"
-                className="btn btn-outline-white btn-lg"
-              >
-                Start free tutorial
-                <i className="fas fa-graduation-cap ml-2"></i>
-              </a>
+              </h1>
             </div>
           </div>
         </div>
