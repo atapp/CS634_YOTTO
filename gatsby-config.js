bd8094fcb2d70b0806af5f25518a89853764327d
atapp <sihogg@bu.edu>
2019-12-10 21:06:32 -0500

    image cahnges

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,11 @@
 // TODO: dear user, please change this to your own instance
 const netlifyInstance = "https://cs634-group1-hogg.netlify.com"
-if (netlifyInstance === "https://cs634-group1-hogg.netlify.com") {
-  console.warn(`
-
-**************************
-WARNING: currently using Netlify Identity of https://cs634-group1-hogg.netlify.com
-
-this is only meant for the working demo. if you forked or copied this code, you won't have access to this netlify identity instance
-
-Go to your site, enable Netlify Identity, and paste that string here
-
-
-More docs: https://www.netlify.com/docs/identity/
-
-**************************
-`)
-}
 
 module.exports = {
   siteMetadata: {
-    title: "JAMstack Hackathon Starter",
-    description: `Kick off your next, great Gatsby app.`,
-    author: `@swyx`,
+    title: "CS634-Project-Hogg",
+    description: `Making a site for Yotto`,
+    author: `@ataap`,
   },
 
   plugins: [
@@ -43,7 +27,5 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
   ],
 }
